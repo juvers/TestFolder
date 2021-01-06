91f926a4b10c577a532298ceccf64cff8cddd9d7
Jude K <judekuti@gmail.com>
2021-01-05 23:18:21 -0600

    Add curry and closure

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const Duplicate = (n) => {
 	return Math.pow(n, n)
 };
 
+const Closure = (x,y) => {
+	return function (z){
+		return z + x * y
+	}
+}
 
+const Main = Closure(3,9);
+console.log(Main(12))
+
+console.log(Closure(6,7)(5))
 console.log(Duplicate(5))
